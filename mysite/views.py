657d988ec983c0b5484497bb45211f26d694f0fb
Khudoteplov <d-n-khudoteplov@yandex.ru>
2025-04-11 10:26:43 +0300

    add base html page

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.core.cache import cache
+from . import characters_db
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "home.html", context={"items": characters_db.get_characters_for_table()})
